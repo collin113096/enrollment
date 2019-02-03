9ee8e850b875ef21749f09f10567454a65544ac1
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-02 22:08:42 -0800

    Create StudentSeeder

--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -12,6 +12,28 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-     
+     	$student = Student::create([
+            'first_name' => 'Jiyo Collin',
+            'middle_name' => 'Babaan',
+            'last_name' => 'Pagtananan',
+            'home_address' => 'Brgy.Cabanbanan Pagsanjan, Laguna',
+            'birth_date' => '1996-11-30',
+            'birth_place' => 'Pagsanjan, Laguna',
+            'religion' => 'Iglesia Ni Cristo',
+            'gender' => 'male',
+            'grade' => 'Grade 1',
+     	]);
+
+        $student->father()->create([
+                'name' => 'Neil Pagtananan',
+                'contact' => '09508223452',
+                'occupation' => 'Farmer',
+            ]);
+
+        $student->mother()->create([
+                'name' => 'Laarni Pagtananan',
+                'contact' => '09508223452',
+                'occupation' => 'Housewife',
+            ]);
     }
 }
