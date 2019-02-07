c87f255006920387f39b53d0ce19ed3da248e10b
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-07 13:00:35 -0800

    Fee Edit

--- resources/views/register/edit.blade.php
+++ resources/views/register/edit.blade.php
@@ -1,52 +1,53 @@
-@extends('template')
+@extends('edit_form')
 @section('title','Edit Student Registration')
-@section('content')
-<form class="col-md-8" method="post" action="/registers/{{ $register->id }}">
-@csrf
-@method('PATCH')
-<div class="card">
-	<div class="card-header">
-		Edit Student Registration
-	</div>
-	<div class="card-body">
+@section('card_header', 'Edit Student Registration')
 
-		<!-- STUDENT LRN -->
-		<div class="form-row form-group">
-			<label class="col-md-4 col-form-label text-md-right"><strong>Student LRN</strong></label>
-			<div class="col-md-6">
-				<input class="form-control" type="text" name="student_lrn" value="{{ $register->student_lrn }}">
-			</div>
-		</div>
+@section('card_body')
 
-		<!-- SCHOOL YEAR -->
-		<div class="form-row form-group">
-			<label class="col-md-4 col-form-label text-md-right"><strong>School Year</strong></label>
-			<div class="col-md-6">
-				<input class="form-control" type="text" name="school_year" value="{{ $register->school_year }}">
-			</div>
-		</div>
+	<!-- STUDENT LRN -->
+	@text
+		@slot('label')
+			Student LRN
+		@endslot
+		@slot('name')
+			student_lrn
+		@endslot
+		@slot('value')
+			{{ $register->student_lrn }}
+		@endslot
+	@endtext
 
-		<!-- SECTION -->
-		<div class="form-row form-group">
-			<label class="col-md-4 col-form-label text-md-right"><strong>Section</strong></label>
-			<div class="col-md-6">
-				<select class="custom-select" name="section_id">
-					<option value="" hidden>Choose</option>
-					@foreach($sections as $section)
-					<option value="{{ $section->id }}" {{ $section->id == $register->section_id ? 'selected':'' }}>{{ $section->name }}</option>
-					@endforeach
-				</select>
-			</div>
-		</div>
-	
-	<!-- END OF CARD BODY -->
-	</div>
+	<!-- SCHOOL YEAR -->
+	@text
+		@slot('label')
+			School Year
+		@endslot
+		@slot('name')
+			school_year
+		@endslot
+		@slot('value')
+			{{ $register->school_year }}
+		@endslot
+	@endtext
 
-	<!-- CARD FOOTER -->
-	<div class="card-footer d-flex justify-content-end">
-		<a class="btn btn-light mr-1" href="/registers">Cancel</a>
-			<button class="btn btn-success" type="submit"><i class="fas fa-save"></i> Save</button>
-	</div>
-</div>
-</form>
+	<!-- SECTION -->
+	@select
+		@slot('label')
+			Section
+		@endslot
+		@slot('name')
+			section_id
+		@endslot
+		@foreach($sections as $section)
+			<option value="{{ $section->id }}" {{ $section->id == $register->section_id ? 'selected':'' }}>{{ $section->name }}</option>
+		@endforeach		
+	@endselect
+
+	@action
+		/registers
+	@endaction
+@endsection
+
+@section('form')
+	/registers/{{ $register->id }}
 @endsection
