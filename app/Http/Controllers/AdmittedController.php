5d3ddf69702c97a9d70f8fafaf1969675131f6bd
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-07 15:10:03 -0800

    Only Retrieve those students who are admitted and not registered

--- app/Http/Controllers/AdmittedController.php
+++ app/Http/Controllers/AdmittedController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Student;
+use App\Register;
 
 class AdmittedController extends Controller
 {
     
     public function index()
     {
-    	$admittedApplicants = Student::where('admitted',1)->paginate(8);
+    	$admittedApplicants = DB::table('students')
+    	->where('admitted',1)
+    	->whereNotIn('id',DB::table('registers')->pluck('id'))
+    	->paginate(8);
     	return view('admitted.index')->with(compact('admittedApplicants'));
     }
 }
