1e22fc7def0631d8cd631c611d5eb309784fbcb4
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-10 00:26:48 -0800

    Only Authenticated User can go to Registrar Page

--- routes/web.php
+++ routes/web.php
@@ -21,33 +21,35 @@ Route::post('/application_form','ApplicationFormController@store');
 Route::get('/documents/{student}','DocumentController@create');
 Route::post('/documents/{student}','DocumentController@store');
 
-Route::get('/applicants','AdmissionController@index');
-Route::get('/applicant/{student}/information','AdmissionController@show');
-Route::patch('/applicants/{student}','AdmissionController@update');
-Route::get('/admitted/applicants','AdmittedController@index');
+Route::group(['middleware' => 'auth'], function(){
+	Route::get('/applicants','AdmissionController@index');
+	Route::get('/applicant/{student}/information','AdmissionController@show');
+	Route::patch('/applicants/{student}','AdmissionController@update');
+	Route::get('/admitted/applicants','AdmittedController@index');
 
-Route::get('/registers','RegisterController@index');
-Route::get('/registers/{student}','RegisterController@create');
-Route::post('/registers/{student}','RegisterController@store');
-Route::get('/registers/{register}/edit','RegisterController@edit');
-Route::delete('/registers/{register}','RegisterController@destroy');
-Route::patch('/registers/{register}','RegisterController@update');
+	Route::get('/registers','RegisterController@index');
+	Route::get('/registers/{student}','RegisterController@create');
+	Route::post('/registers/{student}','RegisterController@store');
+	Route::get('/registers/{register}/edit','RegisterController@edit');
+	Route::delete('/registers/{register}','RegisterController@destroy');
+	Route::patch('/registers/{register}','RegisterController@update');
 
-Route::resource('/times','TimeController');
-Route::resource('/subjects','SubjectController');
-Route::resource('/rooms','RoomController');
-Route::resource('/teachers','TeacherController');
-Route::resource('/sections','SectionController');
-Route::resource('/grades','GradeController');
-Route::resource('/fees','FeeController');
+	Route::resource('/times','TimeController');
+	Route::resource('/subjects','SubjectController');
+	Route::resource('/rooms','RoomController');
+	Route::resource('/teachers','TeacherController');
+	Route::resource('/sections','SectionController');
+	Route::resource('/grades','GradeController');
+	Route::resource('/fees','FeeController');
 
 
-Route::get('/advisory','AdvisoryController@index');
-Route::get('/advisory/{section}/create','AdvisoryController@create');
-Route::post('/advisory/{section}/assign/{teacher}','AdvisoryController@store');
-Route::get('/advisory/{section}/edit','AdvisoryController@edit');
-Route::patch('/advisory/{section}/assign/{teacher}','AdvisoryController@update');
-Route::delete('/advisory/{section}','AdvisoryController@destroy');
+	Route::get('/advisory','AdvisoryController@index');
+	Route::get('/advisory/{section}/create','AdvisoryController@create');
+	Route::post('/advisory/{section}/assign/{teacher}','AdvisoryController@store');
+	Route::get('/advisory/{section}/edit','AdvisoryController@edit');
+	Route::patch('/advisory/{section}/assign/{teacher}','AdvisoryController@update');
+	Route::delete('/advisory/{section}','AdvisoryController@destroy');
+});
 
 Route::resource('/classes','ClassController');
 Auth::routes();
