89a0d4fb0367f4e4242ef74e70f02733a57df98a
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-16 11:18:11 -0800

    Add Links in Management

--- resources/views/registrar/right_side_navbar.blade.php
+++ resources/views/registrar/right_side_navbar.blade.php
@@ -33,6 +33,15 @@
 	</a>
 </li>
 
+<li class="nav-item">
+	<a href="/applicants" class="nav-link">
+		Suggestion
+		<span class="badge badge-primary">
+			{{ $student->countApplicants() }}
+		</span>
+	</a>
+</li>
+
 <!-- 4TH LIST ITEM -->
 <li class="nav-item dropdown">
 
@@ -43,7 +52,7 @@
 		<i class="fas fa-chalkboard-teacher mr-4"></i>Advisory
 	</a>
 	<a href="/teachers" class="dropdown-item">
-		<i class="fas fa-chalkboard-teacher mr-4"></i>Teacher
+		<i class="fas fa-chalkboard-teacher mr-4"></i>Faculty
 	</a>
 	<a href="/subjects" class="dropdown-item">
 		<i class="fas fa-book mr-4"></i>Subject
@@ -54,12 +63,35 @@
 	</a>
 
 	<div class="dropdown-divider"></div>
-	
+
 	<a href="/grades" class="dropdown-item">
 		<i class="fas fa-clipboard mr-4"></i>Grade
 	</a>
 	<a href="/fees" class="dropdown-item">
 		<i class="fas fa-money-bill-alt mr-4"></i>Fee
+	</a>
+
+	<div class="dropdown-divider"></div>
+
+	<a href="/fees" class="dropdown-item">
+		<i class="fas fa-print mr-4"></i>Print Student Report
+	</a>
+
+	<a href="/fees" class="dropdown-item">
+		<i class="fas fa-print mr-4"></i>Print Enrollment Data
+	</a>
+
+	<a href="/fees" class="dropdown-item">
+		<i class="fas fa-print mr-4"></i>Print Faculty
+	</a>
+
+	<div class="dropdown-divider"></div>
+
+	<a href="/fees" class="dropdown-item">
+		<i class="fas fa-download mr-4"></i>Export Database
+	</a>
+	<a href="/fees" class="dropdown-item">
+		<i class="fas fa-upload mr-4"></i>Import Database
 	</a>
 </div>
 
