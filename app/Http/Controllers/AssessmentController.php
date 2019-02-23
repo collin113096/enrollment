0c21b1ed831fc596716a39ce7dda82cb33b1d094
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-23 01:18:09 -0800

    Error Handling When Assessment Form are not available

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -8,11 +8,15 @@ class AssessmentController extends Controller
 {
     public function download(\App\Student $student, \App\Assessment $assessment)
     {
+    	if(!$student->fee(request('payment_mode')))
+    	{
+    		return view('error_for_assessment');
+    		return view('application.waiting_page', ['student' => $student]);
+    	}
     	$html = view('assessment_form',[
     		'student' => $student, 
     		'fee' => $student->fee(request('payment_mode'))
     	]);
-
     	$assessment->showPdf($html);
     }
 }
---
+++ resources/views/error_for_assessment.blade.php
@@ -0,0 +1,7 @@
+@extends('enrollee.template')
+@section('body')
+<div class="alert alert-danger border-danger">
+	Assessment form for this particular payment mode and grade are not yet specified.
+	<a href="/application_form">Go Back</a>
+</div>
+@endsection
