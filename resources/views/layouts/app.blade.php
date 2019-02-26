2d62731e90598f22a7683e66f5130f0fd4b8028c
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-25 20:54:28 -0800

    Add Chart Js Script and Change the registrar default Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 </head>
 <body>
     <div id="app">
