82856e718384ea12831805576b767eafca517f22
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-02-26 20:00:55 -0800

    FeeSeeder

--- database/seeds/FeeSeeder.php
+++ database/seeds/FeeSeeder.php
@@ -12,7 +12,7 @@ class FeeSeeder extends Seeder
      */
     public function run()
     {
-        $fees = [
+        $cashes = [
         	['grade_id' => 1 , 'payment_mode' => 'Cash', 'registration' => 1500, 'tuition' => 1000, 'misc' => 3000, 'computer' => 0, 'internet' => 0],
         	['grade_id' => 2 , 'payment_mode' => 'Cash', 'registration' => 1500, 'tuition' => 1000, 'misc' => 3000, 'computer' => 0, 'internet' => 0],
         	['grade_id' => 3 , 'payment_mode' => 'Cash', 'registration' => 1500, 'tuition' => 1000, 'misc' => 3000, 'computer' => 0, 'internet' => 0],
@@ -30,9 +30,34 @@ class FeeSeeder extends Seeder
         	['grade_id' => 13 , 'payment_mode' => 'Cash', 'registration' => 1500, 'tuition' => 7700, 'misc' => 3500, 'computer' => 1300, 'internet' => 600],
         ];
 
-        foreach($fees as $fee)
+         $installments = [
+            ['grade_id' => 1 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 100, 'misc' => 3000, 'computer' => 0, 'internet' => 0],
+            ['grade_id' => 2 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 100, 'misc' => 3000, 'computer' => 0, 'internet' => 0],
+            ['grade_id' => 3 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 600, 'misc' => 3000, 'computer' => 60, 'internet' => 0],
+
+            ['grade_id' => 4 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 600, 'misc' => 3000, 'computer' => 60, 'internet' => 0],
+            ['grade_id' => 5 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 600, 'misc' => 3000, 'computer' => 60, 'internet' => 0],
+            ['grade_id' => 6 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 600, 'misc' => 3000, 'computer' => 60, 'internet' => 0],
+            ['grade_id' => 7 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 600, 'misc' => 3000, 'computer' => 60, 'internet' => 0],
+            ['grade_id' => 8 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 600, 'misc' => 3000, 'computer' => 60, 'internet' => 0],
+            ['grade_id' => 9 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 770, 'misc' => 3500, 'computer' => 130, 'internet' => 0],
+
+            ['grade_id' => 10 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 770, 'misc' => 3500, 'computer' => 130, 'internet' => 60],
+            ['grade_id' => 11 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 770, 'misc' => 3500, 'computer' => 130, 'internet' => 60],
+            ['grade_id' => 12 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 770, 'misc' => 3500, 'computer' => 130, 'internet' => 60],
+            ['grade_id' => 13 , 'payment_mode' => 'Installment', 'registration' => 1500, 'tuition' => 770, 'misc' => 3500, 'computer' => 130, 'internet' => 60],
+        ];
+
+        foreach($cashes as $cash)
         {
-        	Fee::create($fee);
+        	Fee::create($cash);
         }
+
+        foreach($installments as $installment)
+        {
+            Fee::create($installment);
+        }
+
+
     }
 }
